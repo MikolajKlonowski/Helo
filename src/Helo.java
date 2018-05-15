803141d4a90fdd03942511688873f2b97adf7402
Mikołaj Klonowski <nick-93@o2.pl>
2018-05-15 18:57:57 +0200

    Pierwszy kod JAVA

---
+++ src/Helo.java
@@ -0,0 +1,20 @@
+class Helo {
+    
+    public static void main(String[] args) {
+        System.out.println("abc1");
+        System.out.println("abc2");
+        //to widze tylko ja !!
+        System.out.println("abc3");
+        /*
+        dfsdfs
+         */
+        System.out.println("abc4");
+
+
+
+    }
+
+
+
+
+}
